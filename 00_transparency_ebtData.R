c68cba56270533ee0ac84a091de4c21dce2e364d
aassumpcao <andre.assumpcao@gmail.com>
2018-11-29 12:13:18 -0500

    test

---
+++ 00_transparency_ebtData.R
@@ -0,0 +1,18 @@
+################################################################################
+# active passive transparency paper
+# audit data wrangling
+
+
+# by andre.assumpcao@gmail.com
+
+# import statements
+library(here)
+library(tidyverse)
+library(magrittr)
+library(readxl)
+
+# load datasets
+load('audit.dataset.Rda')
+load('ibge.dataset.Rda')
+
+# what is going here
