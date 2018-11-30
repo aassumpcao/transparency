5e04393e3306f8df030d97abb9f506abece87811
aassumpcao <andre.assumpcao@gmail.com>
2018-11-30 13:09:11 -0500

    downloading reports

--- 00_transparency_sanctionsRata.R
+++ 00_transparency_sanctionsRata.R
@@ -18,4 +18,35 @@ library(magrittr)
 library(readxl)
 
 ################################################################################
-#
+# load data on police cooperation with CGU
+rde <- read_excel('LAI 00075.001622-2018-19.xlsx')
+
+# create year of investigation variable. we extract the year from the case
+#   unique id. if the case unique id is missing, i recover from the year of
+#   investigation variable
+# extract year when case id is not standardized to 17-digit id
+date.fix1 <- rde %>%
+  filter(nchar(Nr_Processo) < 17) %>%
+  mutate(rde.year = substr(Nr_Ordem_Servico, 1, 4))
+
+# extract year when the case is perfectly coded as the standard 17-digit id
+date.fix2 <- rde %>%
+  filter(nchar(Nr_Processo) == 17) %>%
+  mutate(rde.year = substr(Nr_Processo, 12, 15)) %>%
+  filter(rde.year %in% c(2003:2018))
+
+# extract year when case does have 17 digits but does not match standard
+date.fix3 <- rde %>%
+  filter(nchar(Nr_Processo) == 17) %>%
+  filter(!(substr(Nr_Processo, 12, 15) %in% c(2003:2018))) %>%
+  mutate(rde.year = substr(Nr_Ordem_Servico, 1, 4))
+
+# bind them together. in the process, we drop the reports on the state capitals
+# that have nothing to do with police investigations.
+rde <- rbind(date.fix1, date.fix2, date.fix3)
+
+# remove previous temporary objects
+rm(list = objects(pattern = '\\.fix'))
+
+# drop reports that are not originated by police investigations
+rde %<>% filter(!is.na(Demanda))
