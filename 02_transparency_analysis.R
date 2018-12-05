a54a30a34b549ac5d9e48548d11ee551ece59e5e
aassumpcao <andre.assumpcao@gmail.com>
2018-12-05 12:36:30 -0500

    produced first table

--- 02_transparency_analysis.R
+++ 02_transparency_analysis.R
@@ -69,11 +69,100 @@ t.labels  <- c('Active Transparency', 'Passive Transparency',
 
 ################################################################################
 # create dir for prospectus
-dir.create('./proposal')
+dir.create('./proposal3')
 
-# subset dataframe in three
-analysis %$%
-  {t.test(mun.urbanpop ~ .$ebt.treatment,
-          var.equal = FALSE,
-          conf.level = .90)} %>%
-  str()
+# produce means, difference in means and p.values for all variables so that i
+# print the descriptive statistics table
+for (i in seq(covariates)) {
+
+  # run ttest for each treatment arm independently
+  column1 <- paste0(covariates[i], '~ audit.treatment') %>%
+             as.formula() %>%
+             t.test(data = mutate(analysis, mun.income = log(mun.income)),
+                    conf.level = .95) %>%
+             .[c('estimate', 'p.value')] %>%
+             unlist() %>%
+             unname()
+
+  # run ttest for each treatment arm independently
+  column2 <- paste0(covariates[i], '~ ebt.treatment') %>%
+             as.formula() %>%
+             t.test(data = mutate(analysis, mun.income = log(mun.income)),
+                    conf.level = .95) %>%
+             .[c('estimate', 'p.value')] %>%
+             unlist() %>%
+             unname()
+
+  # run ttest for each treatment arm independently
+  column3 <- paste0(covariates[i], '~ double.treatment') %>%
+             as.formula() %>%
+             t.test(data = mutate(analysis, mun.income = log(mun.income)),
+                    conf.level = .95) %>%
+             .[c('estimate', 'p.value')] %>%
+             unlist() %>%
+             unname()
+
+  # compute difference in means and keep p-values
+  column1[3:4] <- c(column1[1] - column1[2], column1[3])
+  column2[3:4] <- c(column2[1] - column2[2], column2[3])
+  column3[3:4] <- c(column3[1] - column3[2], column3[3])
+
+  # build row with such information
+  row1 <- c(column1[1:3], column2[1:3], column3[1:3])
+  row2 <- c('', '', column1[4], '', '', column2[4], '', '', column3[4])
+
+  # bind into dataset
+  if (i == 1) {table <- tibble(row1, row2)}
+  else        {table <- bind_cols(table, row = row1, row = row2)}
+
+  # transpose table at the end of the loop
+  if (i == length(covariates)){table <- as.tibble(data.table::transpose(table))}
+}
+
+# remove unnecessary objects
+rm(list = objects(pattern = 'column|^i$|row'))
+
+# create vectors with the number of observations for descriptive stats table
+audit.obs  <- analysis %$% table(audit.treatment)  %>% unlist() %>% unname()
+ebt.obs    <- analysis %$% table(ebt.treatment)    %>% unlist() %>% unname()
+double.obs <- analysis %$% table(double.treatment) %>% unlist() %>% unname()
+
+# create additional rows (labels and number of observations)
+labels.row <- rep(c('Control', 'Treatment', 'Difference'), 3)
+sample.row <- c(audit.obs, '', ebt.obs, '', double.obs, '') %>%
+              {setNames(as.list(.), paste0('V', 1:9))} %>%
+              as.tibble()
+
+# insert rows
+table %<>%
+  mutate_all(as.numeric) %>%
+  mutate_all(round, digits = 3) %>%
+  mutate_all(as.character) %>%
+  bind_rows(sample.row)
+
+# set col and row names
+names(table) <- labels.row
+table$Variables <- c(cov.labels[1], NA, cov.labels[2], NA, cov.labels[3], NA,
+                     cov.labels[4], NA, cov.labels[5], NA, cov.labels[6], NA,
+                     cov.labels[7], NA, cov.labels[8], NA, cov.labels[9], NA,
+                     cov.labels[10], NA, cov.labels[11], NA, 'Sample Size')
+
+# print table
+xtable::xtable(
+  # table object
+  table,
+  # styling arguments
+  caption = 'Descriptive Statistics by Treatment Condition',
+  label = 'tab:descriptivestats3',
+  align = rep('r', 11),
+  digits = 3,
+  display = rep('s', 11)
+) %>%
+# xtable::print.xtable(
+#   # styling arguments
+#   file = './proposal3/tab_sumstats.tex',
+#   table.placement = '!htbp',
+#   caption.placement = 'top',
+#   hline.after = c(-1, -1, 0, 22, 23, 23),
+#   print.results = TRUE
+# )
